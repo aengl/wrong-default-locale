8a2bc3ee96b261e50a0621ba83fd28ae6af1a662
Lynn Smeria <ae@cephea.de>
2021-01-09 19:09:01 +0200

    Show locale from router

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import { GetStaticProps, InferGetStaticPropsType } from "next";
 import { FunctionComponent } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Index: FunctionComponent<
   InferGetStaticPropsType<typeof getStaticProps>
 > = ({ locale, defaultLocale }) => {
+  const {
+    locale: routerLocale,
+    defaultLocale: routerDefaultLocale,
+  } = useRouter();
   return (
     <div className="root">
       <h1>
@@ -17,6 +22,8 @@ const Index: FunctionComponent<
       </h1>
       <pre>locale: {locale}</pre>
       <pre>defaultLocale: {defaultLocale}</pre>
+      <pre>routerLocale: {routerLocale}</pre>
+      <pre>routerDefaultLocale: {routerDefaultLocale}</pre>
       <Link href="/about">
         {
           {
