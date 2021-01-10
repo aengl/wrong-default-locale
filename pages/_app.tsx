cad06c8a5da8192f52cfa4b05c84c55c85bf434f
Lynn Smeria <ae@cephea.de>
2021-01-10 19:08:35 +0200

    Remove custom app/document

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { AppProps } from "next/app";
-import { Router } from "next/router";
-
-Router.events.on("routeChangeComplete", () => {});
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
